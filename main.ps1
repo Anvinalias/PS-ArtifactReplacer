1951b0a71f5e6916aaa2271401871325296e7792
Anvinalias <anvinkaippillil123@gmail.com>
2025-04-05 18:59:31 +0530

    Seperate Unzip control to unzip.ps1 file in Invoke-UnzipFlow function

--- main.ps1
+++ main.ps1
@@ -7,26 +7,4 @@ $config.LogPath
 # Load helper script
 . "scripts/unzip.ps1"
 
-# Get all application folders under BuildPath
-$appFolders = Get-ChildItem -Path $config.BuildPath -Directory
-
-foreach ($app in $appFolders) {
-    # Get version folders inside each app folder
-    $versionFolders = Get-ChildItem -Path $app.FullName -Directory
-
-    foreach ($version in $versionFolders) {
-        # Prompt the user to continue or skip
-        Write-Host "`nFound version folder: $($version.FullName)" -ForegroundColor Cyan
-        $response = Read-Host "Do you want to process this folder? (Y/N)"
-
-        if ($response -eq "Y" -or $response -eq "y") {
-            # Call your unzip function here
-            Expand-BuildFiles -VersionFolderPath $version.FullName
-        } else {
-            Write-Host "Skipping $($version.Name)`n" -ForegroundColor Yellow
-        }
-    }
-}
-
-
-
+Invoke-UnzipFlow -BuildPath $config.BuildPath
--- scripts/unzip.ps1
+++ scripts/unzip.ps1
@@ -33,4 +33,36 @@ function Expand-BuildFiles {
     
     }
 
+}  
+
+function Invoke-UnzipFlow {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$BuildPath
+    )
+    
+    # Get all application folders under BuildPath
+    $appFolders = Get-ChildItem -Path $config.BuildPath -Directory
+
+    foreach ($app in $appFolders) {
+        # Get version folders inside each app folder
+        $versionFolders = Get-ChildItem -Path $app.FullName -Directory
+
+        foreach ($version in $versionFolders) {
+            # Prompt the user to continue or skip
+            Write-Host "`nFound version folder: $($version.FullName)" -ForegroundColor Cyan
+            $response = Read-Host "Do you want to process this folder? (Y/N)"
+
+            if ($response -eq "Y" -or $response -eq "y") {
+                # Call your unzip function here
+                Expand-BuildFiles -VersionFolderPath $version.FullName
+            }
+            else {
+                Write-Host "Skipping $($version.Name)`n" -ForegroundColor Yellow
+            }
+        }
+    }
+
 }
+
+
