71e5c5da01f9eba8e700f9b6c588fbe83461a170
Anvinalias <anvinkaippillil123@gmail.com>
2025-04-05 18:13:33 +0530

    Replace static path with dynamic path

--- main.ps1
+++ main.ps1
@@ -7,8 +7,18 @@ $config.LogPath
 # Load helper script
 . "scripts/unzip.ps1"
 
-# Example path to pass
-$versionFolder = "C:\Users\anvin\Desktop\Practice\Experiment\Compute_hash\Buildfiles\CrewingPALApp\4.0.0.3"
+# # Example path to pass
+# $versionFolder = "C:\Users\anvin\Desktop\Practice\Experiment\Compute_hash\Buildfiles\CrewingPALApp\4.0.0.3"
+
+# Define the app name and version dynamically
+$appName = "CrewingPALApp"
+$version = "4.0.0.3"
+
+# Combine parts to get full version folder path
+$versionFolder = Join-Path $config.BuildPath "$appName\$version"
+
+# Output to check
+Write-Host "Version folder path: $versionFolder"
 
 # Call the Unzip function
 Unzip -VersionFolderPath $versionFolder
