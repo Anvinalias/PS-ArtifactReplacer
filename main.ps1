86562a6d8c0eefba6db75f934277be7a334ac0df
Anvinalias <anvinkaippillil123@gmail.com>
2025-04-05 17:46:42 +0530

    Call Unzip function by passing the path

--- main.ps1
+++ main.ps1
@@ -4,3 +4,13 @@ $config = Get-Content -Raw -Path "./config.json" | ConvertFrom-Json
 $config.BuildPath
 $config.LogPath
 
+# Load helper script
+. "scripts/unzip.ps1"
+
+# Example path to pass
+$versionFolder = "C:\Users\anvin\Desktop\Practice\Experiment\Compute_hash\Buildfiles\CrewingPALApp\4.0.0.3"
+
+# Call the Unzip function
+Unzip -VersionFolderPath $versionFolder
+
+
