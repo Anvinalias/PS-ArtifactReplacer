8fbf858706d3cfbe31c808284fa87117356e84d3
Anvinalias <anvinkaippillil123@gmail.com>
2025-05-17 11:40:58 +0530

    Add function Write-LogBanner for log titles
    This improves log readability

--- scripts/logging.ps1
+++ scripts/logging.ps1
@@ -22,3 +22,20 @@ function Write-Log {
     Write-Host $logEntry
     Add-Content -Path $LogFile -Value $logEntry
 }
+
+
+function Write-LogBanner {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Title,
+
+        [Parameter(Mandatory = $true)]
+        [string]$LogFile
+    )
+
+    $banner = "==== $Title ===="
+
+    # Console + Log File
+    Write-Host "`n$banner" -ForegroundColor Cyan
+    Add-Content -Path $LogFile -Value "`n$banner"
+}
